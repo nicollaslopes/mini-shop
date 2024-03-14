78c65f20c3ed4845122aec3f8c37b0c48331fbfd
nicollas.lopes <nicollaslopesn@gmail.com>
2024-03-13 21:08:36 -0300

    feat: creating transaction class

---
+++ src/database/Transaction.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace app\database;
+
+use app\database\Connection;
+
+use PDO;
+
+class Transaction
+{
+    private static ?PDO $conn = null;
+
+    public static function getConnection()
+    {
+        if (self::$conn) {
+            return self::$conn;
+        }
+    }
+
+    public static function open()
+    {
+        self::$conn = Connection::connect();
+        self::$conn->beginTransaction();
+    }
+
+    public static function rollback()
+    {
+        if (self::$conn) {
+            self::$conn->rollback();
+            self::$conn = null;
+        }
+    }
+
+    public static function close()
+    {
+        if (self::$conn) {
+            self::$conn->commit();
+            self::$conn = null;
+        }
+    }
+}
